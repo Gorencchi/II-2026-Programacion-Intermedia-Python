4b802d9f55e5bcbb3554178bbc7f181d29b5e2ca
gorencchi <amayaodayaka@gmail.com>
2026-08-22 11:58:55 -0600

    .

--- Clase 04/actividad.py
+++ Clase 04/actividad.py
@@ -6,12 +6,14 @@ print(df)
 #Borrar filas con valores vacios :pp
 df.fillna(0, inplace=True)
 
+#reemplazar valores vacios por la media de la columna :pp
+df.fillna({col: df[col].mean() for col in df.columns if df[col].isnull().any()}, inplace=True)
+
 #valores erroneos, se corrige el formato de la columna de edad :pp
 df.dropna(subset=["Edad"], inplace=True)
 
-#Reemplazar valores erroneos de edad mayores a 100 por 100 y menores a 0 por 0 :pp
-df.loc[df["Edad"] > 100, "Edad"] = 100
-df.loc[df["Edad"] < 0, "Edad"] = 0
+#Reemplazar valores erroneos :pp
+
 #correlaciones
 df.corr()
 print("Correlacion entre edad y calificacion:", df["Edad"].corr(df["Calificacion"]))
